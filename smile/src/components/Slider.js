cad7d7d988b4ae60b1be4fb04f5917d5b5e5a00b
ukalelei <uvioletl@hotmail.com>
2022-05-18 22:48:56 -0500

    import img

--- smile/src/components/Slider.js
+++ smile/src/components/Slider.js
@@ -1,9 +1,15 @@
 import React from "react";
+import img1 from "../assets/img1.jpg";
+import img2 from "../assets/img2.jpg";
+import img3 from "../assets/img3.jpg";
+import img4 from "../assets/img4.jpg";
+
+const images = [img1, img2, img3, img4];
 
 const Slider = () => {
     return(
         <div className="image-container">
-            <img />
+            <img alt="" src="[0]" />
         </div>
     )
 }
